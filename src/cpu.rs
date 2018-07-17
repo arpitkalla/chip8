71443082cade5ef1ebc3d132f706c145a5073a65
arpit-kalla <ak2242@cornell.edu>
2018-07-16 21:50:32 -0400

    Adding opcodes and display

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,16 +1,23 @@
-struct Cpu {
-	pc: u16,
-	i: u16,
-	memory: [u8; 4096],
-	v: [u8; 16],
-	stack: [u16; 16],
-	sp: u8,
-	dt: u8,
-	st: u8
+extern crate rand;
+use rand::Rng;
+
+pub struct Cpu {
+	pub pc: u16,
+	pub i: u16,
+	pub memory: [u8; 4096],
+	pub v: [u8; 16],
+	pub stack: [u16; 16],
+	pub sp: u8,
+	pub dt: u8,
+	pub st: u8,
+	pub display: [[u8; 64]; 32],
+	pub key: [u8; 16]
 }
 
 impl Cpu {
-	fn init(&mut self) {
+
+
+	pub fn reset(&mut self) {
 		// Initialize pc, index, stack pointer
 		self.pc = 0x200;
 		self.i = 0;
@@ -29,28 +36,150 @@ impl Cpu {
 		// Initialize timers
 		self.dt = 60;
 		self.st = 0;
+
+		self.display = [[0; 64]; 32];
+		self.key = [0; 16];
 	}
 
-	fn emulate_cycle(&mut self) {
-		// Fetch opcode
-		let opcode: u16 = (self.memory[self.pc as usize] as u16) << 8 | (self.memory[(self.pc + 1) as usize] as u16);
+	pub fn load_game(&mut self, name: &str) {
 
+	}
+
+	pub fn emulate_cycle(&mut self) {
+		// Fetch opcode
+		// let _opcode: u16 = (self.memory[self.pc as usize] as u16) << 8 | (self.memory[(self.pc + 1) as usize] as u16);
+		let _opcode: u16 = 0xC0FF;
+		
 		// Decode opcode
+		CHIP8_TABLE[((_opcode & 0xF000) >> 12) as usize](self ,_opcode);
 
 		// Update timers
-		if(self.dt > 0) {
-			--self.dt;
+		if self.dt > 0 {
+			self.dt -= 1;
 		}
 
-		if(self.st > 0) {
-			if(self.st == 1) {
+		if self.st > 0 {
+			if self.st == 1 {
 				println!("BEEP!");
 			}
-			--self.st;
+			self.st -= 1;
 		}
+	}
+
+}
+
+fn empty(cpu: &mut Cpu, opcode: u16) {
+	match opcode & 0x00FF {
+		0xE0 => cpu.display = [[0; 64]; 32],
+		0xEE => {
+			cpu.pc = cpu.sp as u16;
+			cpu.sp -= 1;
+		},
+		_ => ()
+	}
+} 
+
+fn arithmetic(cpu: &mut Cpu, opcode: u16) {
+	let x: usize = ((opcode & 0x0F00) >> 8) as usize;
+	let y: usize = ((opcode & 0x00F0) >> 4) as usize;
+
+	match opcode & 0x000F {
+		0 => cpu.v[x] = cpu.v[y],
+		1 => cpu.v[x] = cpu.v[x] | cpu.v[y],
+		2 => cpu.v[x] = cpu.v[x] & cpu.v[y],
+		3 => cpu.v[x] = cpu.v[x] ^ cpu.v[y],
+		4 => {
+			cpu.v[15] = if cpu.v[y] > !cpu.v[x] { 1 } else { 0 };  
+			cpu.v[x] = cpu.v[x] + cpu.v[y];
+		},
+		5 => {
+			cpu.v[15] = if cpu.v[x] > cpu.v[y] { 1 } else { 0 };  
+			cpu.v[x] = cpu.v[x] - cpu.v[y];
+		},
+		6 => {
+			cpu.v[15] = cpu.v[y] % 2;  
+			cpu.v[x] = cpu.v[y] >> 1;
+		},
+		7 => {
+			cpu.v[15] = if cpu.v[y] > cpu.v[x] { 1 } else { 0 };  
+			cpu.v[x] = cpu.v[y] - cpu.v[x];
+		},
+		14 => {
+			cpu.v[15] = cpu.v[y] >> 7;  
+			cpu.v[x] = cpu.v[y] << 1;
+		},
+		_ => ()
 	}
 }
 
+fn flow(cpu: &mut Cpu, opcode: u16) {
+	let nnn: u16 = (opcode & 0x0FFF) as u16;
+	let x: usize = ((opcode & 0x0F00) >> 8) as usize;
+	let y: usize = ((opcode & 0x00F0) >> 4) as usize;
+	let k: u8 = (opcode & 0x00FF) as u8;
+	let mut rng = rand::thread_rng();
+
+	match (opcode & 0xF000) >> 12 {
+		
+		1 => cpu.pc = nnn,
+		2 => {
+			cpu.i += 1;
+			cpu.pc = nnn;
+		},
+		3 => cpu.pc = if cpu.v[x] == k {cpu.pc + 2} else {cpu.pc},
+		4 => cpu.pc = if cpu.v[x] != k {cpu.pc + 2} else {cpu.pc},
+		5 => cpu.pc = if cpu.v[x] == cpu.v[y] {cpu.pc + 2} else {cpu.pc},
+		6 => cpu.v[x] = k,
+		7 => cpu.v[x] += k,
+		9 => cpu.pc = if cpu.v[x] != cpu.v[y] {cpu.pc + 2} else {cpu.pc},
+		0xA => cpu.i = nnn,
+		0xB => cpu.pc = cpu.v[0] as u16 + nnn,
+		0xC => cpu.v[x] = rng.gen::<u8>() & k,
+		_ => ()
+	}
+}
+
+fn f_type(cpu: &mut Cpu, opcode: u16) {
+	let x: usize = ((opcode & 0x0F00) >> 8) as usize;
+
+	match opcode & 0x00FF {
+		0x07 => cpu.v[x] = cpu.dt,
+		0x0A => {
+			cpu.key = [0; 16];
+			loop {
+
+			}
+		},
+		0x15 => cpu.dt = cpu.v[x],
+		0x1E => cpu.st = cpu.v[x],
+		0x29 => cpu.i += cpu.v[x] as u16,
+		0x33 => {
+			cpu.memory[cpu.i as usize] = cpu.v[x] / 100;
+			cpu.memory[(cpu.i + 1) as usize] = (cpu.v[x] % 100) / 10;
+			cpu.memory[(cpu.i + 2) as usize] = cpu.v[x] % 10;
+		},
+		0x55 => {
+			for index in 0..x {
+				cpu.memory[(cpu.i as usize + index)] = cpu.v[index];
+			}
+		},
+		0x65 => {
+			for index in 0..x {
+				cpu.v[index] = cpu.memory[(cpu.i as usize + index)];
+			}
+		},
+		_ => ()
+	}
+}
+fn cpuNULL(cpu: &mut Cpu, opcode: u16) {
+	println!("Null");
+} 
+
+const CHIP8_TABLE: [fn(&mut Cpu, u16); 16] = [
+	empty, flow, flow, flow, flow, flow, flow, flow, 
+	arithmetic, flow, flow, flow, flow, cpuNULL,
+	cpuNULL,f_type
+];
 const FONT_SET: [u8; 80] = [
 	0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
 	0x20, 0x60, 0x20, 0x20, 0x70, // 1
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,46 @@
+extern crate minifb;
+extern crate rand;
+use minifb::{Key, WindowOptions, Window};
 mod cpu;
 
+const WIDTH: usize = 640;
+const HEIGHT: usize = 320;
 
 fn main() {
-    println!("Hello, world!");
+	// Initialization
+	let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new("Chip8 Emulator - ESC to exit",
+                                 WIDTH,
+                                 HEIGHT,
+                                 WindowOptions::default()).unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    let mut cpu = cpu::Cpu {
+    	pc: 0x200,
+    	i: 0,
+    	sp: 0,
+    	stack: [0; 16],
+    	v: [0; 16],
+    	memory: [0; 4096],
+    	dt: 60,
+    	st: 0,
+    	display: [[0; 64]; 32],
+    	key: [0; 16]
+    };
+
+    // Main loop
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+    	// Run the CPU
+    	cpu.emulate_cycle();
+
+    	// Write to screen
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+        println!("{}", cpu.v[0]);
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window.update_with_buffer(&buffer).unwrap();
+    }
 }
